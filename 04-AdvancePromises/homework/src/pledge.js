5f8ae5eac760f9c983528d4958bab10b2fcf65f1
JuliPerezApiwan <julieta.perezapiwan@gmail.com>
2023-01-02 20:57:33 -0300

    Resuelto

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -9,11 +9,15 @@ function $Promise (executor) {
   
     this._state = 'pending';
     this._value = undefined
+    this._handlerGroups = []
     
+
+
     this._internalResolve = (data) =>{
       if(this._state === "pending") {
         this._state = "fulfilled"
         this._value = data
+        this._callHandlers()
       }
     }
 
@@ -21,13 +25,94 @@ function $Promise (executor) {
     if(this._state === "pending") {
       this._state = "rejected"
       this._value = reason
+      this._callHandlers()
       }
     }
 
+    this.resolve = () => {}
+
     executor (this._internalResolve,this._internalReject) 
     
-  
     }
+
+    
+   $Promise.prototype._callHandlers = function () {
+    while(this._handlerGroups.length) {
+      const grupo = this._handlerGroups.shift();
+        if(this._state === 'fulfilled') {
+          if(grupo.successCb){
+        try {
+        const result = grupo.successCb(this._value)
+
+        if(result instanceof $Promise){
+          return result.then(
+            (value) => grupo.downstreamPromise._internalResolve(value),
+            (reason) => grupo.downstreamPromise._internalReject(reason)
+          )
+        } else {
+          grupo.downstreamPromise._internalResolve(result)
+        }
+      } 
+      catch (error){
+        grupo.downstreamPromise._internalReject(error);
+      }
+      } else {
+        grupo.downstreamPromise._internalResolve(this._value)
+    }    
+  } 
+    else if(this._state === 'rejected' ) {
+      if(grupo.errorCb){
+        try {
+        const result = grupo.errorCb(this._value)
+
+        if(result instanceof $Promise){
+          return result.then(
+            (value) => grupo.downstreamPromise._internalResolve(value),
+            (reason) => grupo.downstreamPromise._internalReject(reason)
+          )
+        } else {
+          grupo.downstreamPromise._internalResolve(result)
+        }
+      } 
+      catch (error){
+        grupo.downstreamPromise._internalReject(error);
+        }
+      } else {
+        grupo.downstreamPromise._internalReject(this._value)
+    }    
+  } 
+    }
+    }
+   
+
+    $Promise.prototype.then = function (successCb, errorCb) {
+        const downstreamPromise = new $Promise (() => {})
+
+
+        this._handlerGroups.push({
+        successCb: typeof successCb === 'function' ? successCb : false,
+        //if (typeof successCb === function) return successCb; else return false,
+        errorCb: typeof errorCb === "function" ? errorCb : false,
+        downstreamPromise,
+      });
+
+      if(this._state !== "pending"){
+        this._callHandlers(this._value);
+      }
+      return downstreamPromise;
+    }
+
+    $Promise.prototype.catch = function(errorCb){
+      return this.then(null, errorCb)
+    }
+
+
+   
+
+
+
+   
+ 
     
 
 
