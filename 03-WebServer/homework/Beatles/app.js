2c98755fce57f5d2b8669fcc12b2fdda745f5fe3
JuliPerezApiwan <julieta.perezapiwan@gmail.com>
2023-01-03 18:51:18 -0300

    Resuelto

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs   = require('fs');
+const { rawListeners } = require('process');
 
 var beatles=[{
   name: "John Lennon",
@@ -22,3 +23,77 @@ var beatles=[{
   profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
 }
 ]
+
+http
+.createServer((req, res) =>{
+const { url } = req
+// switch(url){
+
+//   case "/api":
+//   res.writeHead(200,  { "Content-Type": "application/json" })
+//   res.end(JSON.stringify(beatles));
+//   break;
+
+//   case "/api/John%20Lennon":
+//   res.writeHead(200,  { "Content-Type": "application/json" })
+//   res.end(JSON.stringify(beatles[0]));
+//   break;
+
+//   case "/api/Paul%20McCartney":
+//     res.writeHead(200,  { "Content-Type": "application/json" })
+//     res.end(JSON.stringify(beatles[1]));
+//     break;
+  
+//   case "/api/George%20Harrison":
+//     res.writeHead(200,  { "Content-Type": "application/json" })
+//     res.end(JSON.stringify(beatles[2]));
+//     break;
+  
+//   case "/api/Richard%20Starkey":
+//     res.writeHead(200,  { "Content-Type": "application/json" })
+//     res.end(JSON.stringify(beatles[3]));
+//     break;
+// } 
+
+if(req.url === "/"){
+  res.writeHead(200,  { "Content-type": "text/html" })
+  const text = fs.readFileSync(`./index.html`, "utf-8");
+  res.end(text);
+}
+
+
+const miUrl = req.url.split("/");
+if(miUrl.length < 3) {
+  const beatleName = miUrl[1].replace("%20", " ");
+  const beatle = beatles.filter((elem) => elem.name === beatleName)[0];
+  let template = fs.readFileSync("./beatle.html", "utf-8");
+
+  template = template.replace("{name}", beatle.name);
+  template = template.replace("{birthdate}", beatle.birthdate);
+  template = template.replace("{image}", beatle.profilePic);
+  res.writeHead(200, {"Content-type": "text/html"} ).end(template)
+}
+// revisar porque no me toma el name, pero el birthdate y la imagen si
+
+
+if(req.url === "/api"){
+  res.writeHead(200,  { "Content-type": "application/json" })
+  res.end(JSON.stringify(beatles));
+}
+
+
+const beatlesName = req.url.split("/").pop().replace("%20", " ")
+
+if(req.url.includes("/api") && beatlesName){
+  const beatle = beatles.filter((elem) => elem.name === beatlesName)
+  if(beatle.length === 0) return res.writeHead(404).end("Not Found") // busca el beatle que ingrese y no lo encuentra
+  res.writeHead(200,  { "Content-type": "application/json" })
+  res.end(JSON.stringify(beatle));
+}
+
+
+
+})
+
+
+.listen(3001, "localhost")
--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,55 @@
-var fs  = require("fs")
-var http  = require("http")
+var fs = require("fs")
+var http = require("http")
 
 // Escribí acá tu servidor
+http
+  .createServer((req, res) => {
+    const { url } = req;
+
+    switch (url) {
+      case "/arcoiris_doge":
+        res.writeHead(200, { "Content-Type": "image/jpeg" })
+        const img = fs.readFileSync(__dirname + `/images/arcoiris_doge.jpg`);
+        res.end(img);
+        break;
+
+      case "/badboy_doge":
+        res.writeHead(200, { "Content-Type": "image/jpeg" })
+        const img2 = fs.readFileSync(__dirname + `/images/badboy_doge.jpg`);
+        res.end(img2);
+        break;
+
+      case "/code_doge":
+        res.writeHead(200, { "Content-Type": "image/jpeg" })
+        const img3 = fs.readFileSync(__dirname + `/images/code_doge.jpg`);
+        res.end(img3);
+        break;
+
+      case "/resaca_doge":
+          res.writeHead(200, { "Content-Type": "image/jpeg" })
+          const img4 = fs.readFileSync(__dirname + `/images/resaca_doge.jpg`);
+          res.end(img4);
+          break;
+      
+      case "/retrato_doge":
+          res.writeHead(200, { "Content-Type": "image/jpeg" })
+          const img5 = fs.readFileSync(__dirname + `/images/retrato_doge.jpg`);
+          res.end(img5);
+          break;
+      
+      case "/sexy_doge":
+          res.writeHead(200, { "Content-Type": "image/jpeg" })
+          const img6 = fs.readFileSync(__dirname + `/images/sexy_doge.jpg`);
+          res.end(img6);
+          break;
+    
+      default:
+        res.writeHead(404); 
+		    res.end("404 Not Found"); 
+
+
+    }
+  })
+  .listen(3001, "127.0.0.1");
+
+
